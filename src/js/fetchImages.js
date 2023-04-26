d42b935a70487e5f96d6a5573cd4d799a8027cf2
Olena1010 <ebojcenko55@gmail.com>
2023-04-26 13:46:39 +0300

    edits

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -9,7 +9,7 @@ async function fetchImg(query, page, perPage) {
         const response = await axios.get(
         `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
         );
-        // console.log(response.data)
+
         return response;
         
     }
